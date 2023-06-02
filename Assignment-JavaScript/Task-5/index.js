205cd0068ffbad3239cc4b4301d8d7e05b5bd3b3
Parthiv Konwer <130231099+Rocket-76@users.noreply.github.com>
2023-06-02 19:35:13 +0530

    index.js

---
+++ Assignment-JavaScript/Task-5/index.js
@@ -0,0 +1,22 @@
+// Function to reverse the input
+function reverseInput(event) {
+  event.preventDefault(); // Prevent form submission from refreshing the page
+  
+  // Get the input value
+  var input = document.getElementById('input').value;
+  
+  // Reverse the input
+  var reversedInput = reverseString(input);
+  
+  // Display the reversed input
+  var outputElement = document.getElementById('output');
+  outputElement.innerHTML = 'Reversed: ' + reversedInput;
+}
+
+// Function to reverse a string
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+// Attach form submit event listener
+document.getElementById('reverseForm').addEventListener('submit', reverseInput);
