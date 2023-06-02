8fa809f57419d97c32a86d16ed7a39abb380e09f
Parthiv Konwer <130231099+Rocket-76@users.noreply.github.com>
2023-06-02 19:43:29 +0530

    Create index.js

---
+++ Assignment-JavaScript/Task-6/index.js
@@ -0,0 +1,22 @@
+// Function to sort the letters in alphabetical order
+function sortLetters(event) {
+  event.preventDefault(); // Prevent form submission from refreshing the page
+  
+  // Get the input value
+  var input = document.getElementById('input').value;
+  
+  // Sort the letters in alphabetical order
+  var sortedString = sortString(input);
+  
+  // Display the sorted string
+  var outputElement = document.getElementById('output');
+  outputElement.innerHTML = 'Alphabetical Order: ' + sortedString;
+}
+
+// Function to sort a string in alphabetical order
+function sortString(str) {
+  return str.split('').sort().join('');
+}
+
+// Attach form submit event listener
+document.getElementById('sortForm').addEventListener('submit', sortLetters);
